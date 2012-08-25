deccc54c9b1c2e040faaf15aa6c3f87e854494e8
Clay S <csbuiss@gmail.com>
2012-08-25 10:22:07 -0500

    Initial Commit

---
+++ WordCountB1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WordCountB1
+//
+//  Created by Clayton Selby on 8/25/12.
+//  Copyright (c) 2012 abc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
