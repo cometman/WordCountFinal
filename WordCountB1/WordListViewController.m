e99afb63f6720b6cac9368c6d5e73d6c5f4233d6
Brandon Ashton <ashtonbt1@gmail.com>
2012-08-25 23:05:09 -0500

    Added stub model to WordListViewController

--- WordCountB1/WordListViewController.m
+++ WordCountB1/WordListViewController.m
@@ -8,13 +8,15 @@
 
 #import "WordListViewController.h"
 
-NSInteger const NumDataRows = 5;
-
 @interface WordListViewController ()
+
+@property (nonatomic, strong) NSMutableArray *wordProfiles;
 
 @end
 
 @implementation WordListViewController
+
+@synthesize wordProfiles = _wordProfiles;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,7 +31,13 @@ NSInteger const NumDataRows = 5;
 {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"profile_bg"]];
-    if (NumDataRows <= 3) {
+    _wordProfiles = [@[
+    @[@"Holy Prayers",@"Lord",@"Father God",@"Jesus",@"Father"],
+    @[@"Brandon's List",@"Like",@"Basically",@"And",@"You Know"],
+    @[@"Clay's List",@"Groovy",@"Like Such As",@"And Then",@"Yup"],
+    @[@"Andres's List",@"I'm Mexican!",@"Crazy Apes",@"What?",@"Sure"]] mutableCopy];
+
+    if ([_wordProfiles count] <= 3) {
         UIImage *characters = [UIImage imageNamed:@"characters"];
         UIImageView *imageHolder = [[UIImageView alloc] initWithFrame:CGRectMake(30, 326, 226, 135)];
         imageHolder.image = characters;
@@ -57,7 +65,7 @@ NSInteger const NumDataRows = 5;
     UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:nil];
     
     // Special case for last cell to hold the characters
-    if (NumDataRows > 3 && ([indexPath row] == NumDataRows + 2)) {
+    if ([_wordProfiles count] > 3 && ([indexPath row] == [_wordProfiles count] + 2)) {
         UIView *lastCell = [[UIView alloc] initWithFrame:CGRectMake(0, 0, cell.frame.size.width, cell.frame.size.height)];
         UIImageView *iv = [[UIImageView alloc] initWithFrame:CGRectMake(30, 10, 226, 135)];
         UIImage *characterImage = [UIImage imageNamed:@"characters"];
@@ -88,8 +96,8 @@ NSInteger const NumDataRows = 5;
             break;
             
         default:
-            cell.textLabel.text = @"Someone's List";
-            cell.detailTextLabel.text = @"One  -  Two  -  Three  -  Four";
+            cell.textLabel.text = [NSString stringWithFormat:@"%@", [[_wordProfiles objectAtIndex:[indexPath row] - 2] objectAtIndex:0]];
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%@  -  %@  -  %@  -  %@", [[_wordProfiles objectAtIndex:[indexPath row] - 2] objectAtIndex:1],[[_wordProfiles objectAtIndex:[indexPath row] - 2] objectAtIndex:2],[[_wordProfiles objectAtIndex:[indexPath row] - 2] objectAtIndex:3],[[_wordProfiles objectAtIndex:[indexPath row] - 2] objectAtIndex:4]];
             cell.imageView.image = [UIImage imageNamed:@"profileEdit"];
             break;
     }
@@ -98,16 +106,17 @@ NSInteger const NumDataRows = 5;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    if ([indexPath row] == NumDataRows + 2) {
+    if ([indexPath row] == [_wordProfiles count] + 2) {
         return 145;
     } else return 54;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (NumDataRows > 3)
-        return NumDataRows + 3;
+    int dataRows = [_wordProfiles count];
+    if (dataRows > 3)
+        return dataRows + 3;
     else
-        return NumDataRows + 2;
+        return dataRows + 2;
 }
 
 @end
