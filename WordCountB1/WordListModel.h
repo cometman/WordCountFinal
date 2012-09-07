b2c17a4a629bc01f00511727adb419cdfb600bc2
Clay S <csbuiss@gmail.com>
2012-09-06 20:51:47 -0500

    here is our test

--- WordCountB1/WordListModel.h
+++ WordCountB1/WordListModel.h
@@ -2,8 +2,7 @@
 //  WordListModel.h
 //  WordCountB1
 //
-//  Created by Clayton Selby on 8/29/12.
-//  Copyright (c) 2012 abc. All rights reserved.
+// Anything i want
 //
 
 #import <Foundation/Foundation.h>
